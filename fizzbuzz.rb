63dc24034f578021ba3489a59aea2a160f9c8271
desmond beramendi <desmond7187@gmail.com>
2018-01-12 19:08:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0 
+  if ((num % 5 == 0) && (num % 3 == 0))
+    "FizzBuzz"
+  elsif num % 3 == 0 
     "Fizz"
   elsif num % 5 == 0 
     "Buzz"
-  elsif ((num % 5 == 0) && (num % 3 == 0))
-    "FizzBuzz"
   else 
     return nil 
   end 
